38b7d4dae27975cddd32866c3f9d1c4a36042e06
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-20 20:03:15 +0900

    feat(session): 세션 고정 공격으로부터 보호하기 위해 로그인 시 동일한 세션에 대한 id 변경

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -35,6 +35,15 @@ public class SecurityConfig {
         http
                 .csrf((auth) -> auth.disable());
 
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        http
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());
+
         return http.build();
     }
 }
